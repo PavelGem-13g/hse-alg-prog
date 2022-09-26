e6abd10c8832b59a0106ab4ae09f752d9ad6b6cc
hazkerindii228 <59193313+PavelGem-13g@users.noreply.github.com>
2022-09-26 21:07:37 +0300

    extra-lab-1

--- lab1/main.c
+++ lab1/main.c
@@ -12,14 +12,14 @@ void print_array(int n, double array[]) {
 
 
 int main() {
-    int n, k, start, end, temp;
+    int n, k, start, end, temp, pos_i;
     double R[lmax], x, h, a, srznach, sum;
     {
         printf("Лабораторная работа №1\nВыполнил:Пашенцев Павел Владимирович\n");
         do {
             printf("Введите длину массива R в диапазоне от 2 до %d ", lmax);
             scanf("%d", &n);
-        } while (!(2 < n && n < lmax));
+        } while (!(1 < n && n < lmax));
 
         printf("Введите коэффициент x ");
         scanf("%lg", &x);
@@ -38,7 +38,6 @@ int main() {
     print_array(n, R);
 
     printf("Задача 2 \n");
-    //поиск последнего отрицательного
     int neg_i = -1;
     for (int i = 0; i < n; ++i) {
         if (R[i] < 0) {
@@ -46,60 +45,58 @@ int main() {
         }
     }
     if (neg_i != -1) {
-        //динамический сдвиг чисел
-        int delta = 0;
-
+        k = 0;
         for (int i = 0; i < neg_i; ++i) {
-            if (R[i] > 0) {
-                ++delta;
-            } else {
-                R[i - delta] = R[i];
+            if (R[i] < 0) {
+                R[k] = R[i];
+                ++k;
             }
         }
-        if(delta==0){
-            printf("Нет элементов для удаления");
+        for (int i = neg_i; i < n; ++i,++k) {
+            R[k] = R[i];
+        }
+        neg_i -= n-k;
+        printf("%d %d\n", n, k);
+        if(n==k){
+            printf("Нет элементов для удаления\n");
+            printf("Задача 3 \n");
+            printf("Нельзя посчитать среднее значение по условию задачи\n");
         } else{
-            // сдвиг оставшихся чисел
-            for (int i = neg_i; i < n; ++i) {
-                R[i - delta] = R[i];
-            }
-            neg_i -= delta;
-
-            k = n - delta;
             print_array(k, R);
 
             printf("Задача 3 \n");
-
-            int min_abs_i = 0;
+            pos_i = -1;
             for (int i = 0; i < k; ++i) {
-                min_abs_i = (fabs(R[min_abs_i])> fabs(R[i]))? i:min_abs_i;
+                pos_i = (R[i]>0)? i:pos_i;
             }
-
-            start = neg_i;
-            end = min_abs_i;
-            if(start>end){
-                temp = start;
-                start = end;
-                end = temp;
-            }
-
-            if (end-start > 0) {
-                sum = 0;
-                for (int i = start; i < end + 1; ++i) {
-                    sum += R[i];
+            if(pos_i!=-1){
+                start = neg_i;
+                end = pos_i;
+                if(start>end){
+                    temp = start;
+                    start = end;
+                    end = temp;
                 }
-                srznach = sum / (end-start+1);
-                printf("Среднее арифметическое %lg", srznach);
-            } else {
-                printf("Нельзя посчитать среднее значение");
+                if (end-start > 0) {
+                    sum = 0;
+                    for (int i = start; i < end + 1; ++i) {
+                        sum += R[i];
+                    }
+                    srznach = sum / (end-start+1);
+                    printf("Среднее арифметическое %lg", srznach);
+                } else {
+                    printf("Нельзя посчитать среднее значение");
+                }
+            }
+            else{
+                printf("Нельзя найти последний положительный элемент\n");
+                printf("Нельзя посчитать среднее значение по условию задачи\n");
             }
         }
-
-
     } else {
-        printf("Нет отрицательных значений");
+        printf("Нет отрицательных значений\n");
+        printf("Задача 3 \n");
+        printf("Нельзя посчитать среднее значение по условию задачи\n");
     }
-
-
     return 0;
 }
